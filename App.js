1c82227a3ffe798cdd72ac295b88501eb51419ac
unknown <rizky.fadhlillah3@gmail.com>
2019-10-06 16:00:21 +0700

    add 2.add_todo

--- App.js
+++ App.js
@@ -1,37 +1,73 @@
 import React, { Component } from 'react';
-import { Container, Header, Content, List, ListItem, Text, Left} from 'native-base';
+import { View, StyleSheet} from 'react-native'
+import { Container, Button, Header, Content, List, ListItem, Text, Left, Item, Input} from 'native-base';
 export default class ListItemSelectedExample extends Component {
   constructor(props) {
     super(props)
 
     this.state = {    
-      numbers : 
+      data : 
       [
-        'work',
-        'swim',
-        'study',
-        'sleep',
-        'run'
-      ]
+        {id : 1, value :'work'},
+        {id : 2, value :'swim'},
+        {id : 3, value :'study'},
+        {id : 4, value :'sleep'},
+        {id : 5, value :'run'},
+      ],
+      toDo: ''
+
     }
   }
+
+  handleButton(){
+    if(this.state.toDo === ''){
+      alert('Masukan Input')
+    } else {
+      const inText = {id: this.state.data.length + 1, value : this.state.toDo};
+      const newData = this.state.data.slice(); 
+      newData.push(inText);
+      this.setState({data:newData})
+      this.setState((data) => (data.toDo))
+    
+      this.state.toDo = ''
+    }
+  }
+  
   render() {
     
     
-    const doubled = this.state.numbers.map((number) =>
+    const doubled = this.state.data.map(({value}) =>
     <List>
     <ListItem selected>
       <Left>
-        <Text>{number}</Text>
+        <Text>{value}</Text>
       </Left>
     </ListItem>
     </List>);
           
     return (
       <Container>
-        <Header />
-        <Content>{doubled}</Content>
+       <Header />
+        <Content>
+        <View style={styles.form}>
+            <Item style={styles.addText}>
+              <Input
+                value={this.state.toDo}
+                onChangeText={(text) => this.setState({ toDo: text })}/>
+              <Button small primary 
+               onPress={() => this.handleButton()}>
+                <Text> Add Todo </Text>
+              </Button>
+            </Item>            
+          </View>
+          {doubled}
+        </Content>
       </Container>
     );
   }
 }
+const styles = StyleSheet.create({
+  addText: {
+    marginBottom: 20
+  },
+})

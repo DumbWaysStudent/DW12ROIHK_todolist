ce3171cddbaad3c07857fc089e752d4dc7386d78
unknown <rizky.fadhlillah3@gmail.com>
2019-10-06 22:27:59 +0700

    add 4.todo_is_done

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, StyleSheet} from 'react-native'
+import { View, StyleSheet, CheckBox} from 'react-native'
 import { Container, Icon, Button, Header, Content, List, ListItem, Text, Left, Item, Input} from 'native-base';
 export default class ListItemSelectedExample extends Component {
   constructor(props) {
@@ -8,13 +8,14 @@ export default class ListItemSelectedExample extends Component {
     this.state = {    
       data : 
       [
-        {id : 1, value :'work'},
-        {id : 2, value :'swim'},
-        {id : 3, value :'study'},
-        {id : 4, value :'sleep'},
-        {id : 5, value :'run'},
+        {id : 1, value :'work', isChecked:false},
+        {id : 2, value :'swim', isChecked:false},
+        {id : 3, value :'study', isChecked:false},
+        {id : 4, value :'sleep', isChecked:false},
+        {id : 5, value :'run', isChecked:false},
       ],
       toDo: ''
+      
 
     }
   }
@@ -44,13 +45,32 @@ export default class ListItemSelectedExample extends Component {
     }
   }
   
+  changedCheckbox(idx) {
+    let check
+    this.setState((state)=> {
+      if(state.data[idx].isChecked != true){
+        check = state.data[idx].isChecked =true
+      }else {
+        check = state.data[idx].isChecked =false
+      
+      }
+      return check
+    })        
+  }
+  
   render() {
     
-    const doubled = this.state.data.map((datas, key) =>
+    const doubled = this.state.data.map((datas) =>
     <List>
     <ListItem selected>
       <Left>
-        
+        <View style={{ flexDirection: 'column'}}>
+          <View style={{ flexDirection: 'row' }}>
+          <CheckBox
+            value={datas.isChecked}
+            onValueChange={() => this.changedCheckbox(datas.id-1)}/>
+          </View>
+      </View>
         <Text key={datas.id}>{datas.value}</Text>
       </Left>
         <Icon name='trash' 

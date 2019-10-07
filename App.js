9e02291bde051b1f514ef398afc7352f5874d9a6
unknown <rizky.fadhlillah3@gmail.com>
2019-10-07 14:20:54 +0700

    add 5.update_todo

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { View, StyleSheet, CheckBox} from 'react-native'
-import { Container, Icon, Button, Header, Content, List, ListItem, Text, Left, Item, Input} from 'native-base';
+import { Container, Icon, Button, Header, Content, List, ListItem, Text, Left, Item, Input, Title, Body} from 'native-base';
 export default class ListItemSelectedExample extends Component {
   constructor(props) {
     super(props)
@@ -8,44 +8,76 @@ export default class ListItemSelectedExample extends Component {
     this.state = {    
       data : 
       [
-        {id : 1, value :'work', isChecked:false},
-        {id : 2, value :'swim', isChecked:false},
-        {id : 3, value :'study', isChecked:false},
-        {id : 4, value :'sleep', isChecked:false},
-        {id : 5, value :'run', isChecked:false},
+        {id : 0, value :'work', isChecked:false},
+        {id : 1, value :'swim', isChecked:false},
+        {id : 2, value :'study', isChecked:false},
+        {id : 3, value :'sleep', isChecked:false},
+        {id : 4, value :'run', isChecked:false},
       ],
-      toDo: ''
+      toDo: '',
+      mode: 'Add',
+      idx:''
       
 
     }
   }
 
-  handleAddBtn(){
-    if(this.state.toDo === ''){
-      alert('Masukan Input')
-    } else {
-      const inText = {id: this.state.data.length + 1, value : this.state.toDo};
-      const newData = this.state.data.slice(); 
-      newData.push(inText);
-      this.setState({data:newData})
-      this.setState((data) => (data.toDo))
-    
-      this.state.toDo = ''
-    }
+  begin(){
+    this.setState({toDo:'', mode:'Add',id:''})
   }
-  
-  handleRemoveBtn(idx){
-    for(let i = 0; i < this.state.data.length; i++){
-      if (this.state.data[i].id == (idx)){
+  handleAddBtn(){
+    if(this.state.mode == 'Add'){
+      if(this.state.toDo === ''){
+        alert('Masukan Input')
+      } else {
+        const inText = {id: this.state.data.length, value : this.state.toDo};
+        const newData = this.state.data.slice(); 
+        newData.push(inText);
+        this.setState({data:newData})
+        this.setState((data) => (data.toDo))
+        this.state.toDo = ''
+      }
+    }else if(this.state.mode == 'Edit'){
+      for(let i = 0; i < this.state.data.length; i++){
+        if (this.state.data[i].id == (this.state.idx)){
+          const inText = {
+            id: this.state.idx,
+            value: this.state.toDo,
+            isChecked: false
+          };
         this.setState((state) => {
-          const list = state.data.splice(i,1)
-          return list
+          return state.data[i] = inText
         })
       }
     }
+  this.begin()
+  }
+}
+
+  handleRemoveBtn(idx){
+    if(this.state.mode == 'Edit'){
+      alert('Tidak Bisa Dihapus')
+    }else{
+      for(let i = 0; i < this.state.data.length; i++){
+        if (this.state.data[i].id == (idx)){
+          this.setState((state) => {
+            const list = state.data.splice(i,1)
+            return list
+          })
+        }
+      }
+    }
+  }
+  handleEditBtn(idx){
+    this.setState({ toDo : this.state.data[idx].value, mode: 'Edit', idx:idx})
+  }
+
+  getIndex(id){
+    return this.state.data.findIndex(obj => obj.id === id);
   }
   
-  changedCheckbox(idx) {
+  changedCheckbox(id) {
+    const idx = this.getIndex(id)
     let check
     this.setState((state)=> {
       if(state.data[idx].isChecked != true){
@@ -68,13 +100,13 @@ export default class ListItemSelectedExample extends Component {
           <View style={{ flexDirection: 'row' }}>
           <CheckBox
             value={datas.isChecked}
-            onValueChange={() => this.changedCheckbox(datas.id-1)}/>
+            onValueChange={() => this.changedCheckbox(datas.id)}/>
           </View>
-      </View>
+        </View>
         <Text key={datas.id}>{datas.value}</Text>
       </Left>
       <Icon name='create' 
-        onPress={() => this.handleRemoveBtn(datas.id)} />
+        onPress={() => this.handleEditBtn(datas.id)} />
         <Text>    </Text>     
       <Icon name='trash' 
         onPress={() => this.handleRemoveBtn(datas.id)} />     
@@ -89,10 +121,11 @@ export default class ListItemSelectedExample extends Component {
             <Item style={styles.addText}>
               <Input
                 value={this.state.toDo}
-                onChangeText={(text) => this.setState({ toDo: text })}/>
+                onChangeText={(text) => this.setState({ toDo: text })}
+                placeholder='New ToDo'/>
               <Button small primary 
                onPress={() => this.handleAddBtn()}>
-                <Text> Add Todo </Text>
+                <Text>{this.state.mode}</Text>
               </Button>
             </Item>            
           </View>
